2e5c56fcdcc867ee478a269e8bb0979349cc6ca6
Dernise <dernise@live.fr>
2014-02-02 23:32:36 +0100

    Fixing linux & mac build

--- src/Types.hpp
+++ src/Types.hpp
@@ -1,11 +1,11 @@
 #ifndef _TYPES_H
 #define _TYPES_H
 
-typedef signed char     int8;
-typedef unsigned char	uint8;
-typedef short           int16;
-typedef unsigned short  uint16;
-typedef int             int32;
-typedef unsigned int    uint32;
+//typedef signed char     int8;
+//typedef unsigned char	uint8;
+//typedef short           int16;
+//typedef unsigned short  uint16;
+//typedef int             int32;
+//typedef unsigned int    uint32;
 
 #endif
