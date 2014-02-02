e676b6e0fe2c269e453a52efe932ecc24c30c711
Maxence Henneron <dernise@live.fr>
2014-02-02 22:48:02 +0100

    Log system and rework of the server

--- src/Log.hpp
+++ src/Log.hpp
@@ -1,13 +1,72 @@
-#define sLog Log::getInstance()
+#ifndef _LOGGING_H
+#define _LOGGING_H
 
-class Log{
+#include <fstream>
+#include <stdio.h>
+#include <time.h>
+#include <stdarg.h>
+#include <iostream>
+#include <string.h>
+#include "Types.hpp"
+
+#if defined (WIN32)
+	#include <SDKDDKVer.h>
+	#include <windows.h>
+#endif
+
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#define sLog Logging::getInstance()
+
+enum LogLevel //Unimplemented: Log level
+{
+    LOG_LVL_MINIMAL = 0, 
+    LOG_LVL_BASIC   = 1,
+    LOG_LVL_DETAIL  = 2,
+    LOG_LVL_DEBUG   = 3
+};
+
+enum Color
+{
+    BLACK,
+    RED,
+    GREEN,
+    BROWN,
+    BLUE,
+    MAGENTA,
+    CYAN,
+    GREY,
+    YELLOW,
+    LRED,
+    LGREEN,
+    LBLUE,
+    LMAGENTA,
+    LCYAN,
+    WHITE
+};
+
+const int Color_count = int(WHITE) + 1;
+
+class Logging{
 public:
-	static Log& getInstance()
+	static Logging& getInstance()
     {
-        static Log instance;
+        static Logging instance;
         return instance;
     }
-    void outString(const char* string);
+	void Initialize(const char* fileName, int logLevel, ...);
+	void outString(const char* str, ...);
+	void outError(const char* str, ...);
+	void outWarning(const char* str, ...);
 private:
-	Log();
+	Logging(){}
+    void SetColor(bool stdout_stream, Color color);
+	std::string getCurrentTime();
+	void ResetColor(bool stdout_stream);
+	int m_logLevel;
+	std::ofstream *m_file;
 };
+
+#endif
---
+++ src/Players.hpp
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include <cstdlib>
+#include <deque>
+#include <iostream>
+#include <list>
+#include <set>
+#include <boost/bind.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/asio.hpp>
+#include <iostream>
+#include "Messages.hpp"
+
+typedef std::deque<chat_message> chat_message_queue;
+
+class Player
+{
+public:
+  virtual ~Player() {}
+  virtual void deliver(const chat_message& msg) = 0;
+};
+
+typedef boost::shared_ptr<Player> chat_participant_ptr;
+
+class Players
+{
+public:
+  void join(chat_participant_ptr participant)
+  {
+    participants_.insert(participant);
+    std::for_each(recent_msgs_.begin(), recent_msgs_.end(),
+        boost::bind(&Player::deliver, participant, _1));
+  }
+
+  void leave(chat_participant_ptr participant)
+  {
+    participants_.erase(participant);
+  }
+
+  void deliver(const chat_message& msg)
+  {
+    recent_msgs_.push_back(msg);
+    while (recent_msgs_.size() > max_recent_msgs)
+      recent_msgs_.pop_front();
+
+    std::for_each(participants_.begin(), participants_.end(),
+        boost::bind(&Player::deliver, _1, boost::ref(msg)));
+  }
+
+private:
+  std::set<chat_participant_ptr> participants_;
+  enum { max_recent_msgs = 100 };
+  chat_message_queue recent_msgs_;
+};
